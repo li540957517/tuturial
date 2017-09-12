1a05fbb6bfab8b4ddf2295e3abee82496eceb5fa
li540957517 <540957517@qq.com>
2017-09-13 06:19:50 +0800

    信息列表展示页 CRUD

--- feedback/urls.py
+++ feedback/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     url(r'^$', view=views.home, name='feedback'),
     url(r'^postdata/$', view=views.get_feedback_data, name='get_feedback_data'),
     url(r'^form/$', view=views.feedback_form, name='feedback_form'),
+    url(r'^list/$', view=views.feedback_list, name='feedback_list'),
+    url(r'^(?P<fid>\d{1,4})/$', view=views.feedback_detail, name='feedback_detail'),
+    url(r'^edit/(?P<fid>\d{1,4})/$', view=views.feedback_edit, name='feedback_edit'),
+    url(r'^del/(?P<fid>\d{1,4})/$', view=views.feedback_delete, name='feedback_delete'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.http import HttpResponse
 from .models import Feedback
 from .forms import FeedbackForm
@@ -49,3 +49,49 @@ def upload_file(f):
     with open('uploads\{}'.format(f.name), 'wb+') as file:
         for chunk in f.chunks():
             file.write(chunk)
+
+
+def feedback_list(request):
+    """信息列表页"""
+    q = request.GET.get('q', '')
+    items = Feedback.objects.all().filter(subject__contains=q).order_by('-posted_time')
+    return render(request, 'feedback/feedback-list.html', {'items': items})
+
+
+def feedback_detail(request, fid):
+    """信息详情展示页"""
+    # feedback = Feedback.objects.get(pk=fid)
+    feedback = get_object_or_404(Feedback, pk=fid)
+    return render(request, 'feedback/feedback-detail.html', {'item': feedback})
+
+
+def feedback_edit(request, fid):
+    """编辑特定id信息"""
+    fb = get_object_or_404(Feedback, pk=fid)
+    form = FeedbackForm(initial={
+        'subject': fb.subject,
+        'category': fb.category,
+        'username': fb.username,
+        'email': fb.email,
+        'screenshot': fb.screenshot,
+        'description': fb.description,
+        'subscription': fb.subscription,
+        'status': fb.status,
+        'posted_time': fb.posted_time,
+    })
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST, request.FILES)
+        if form.is_valid():
+            # fb.subject = form.cleaned_data['subject']
+            for k, v in form.cleaned_data.items():
+                setattr(fb, k, v)
+            fb.save()
+            return redirect(reverse('feedback:feedback_list'))
+    return render(request, 'feedback/feedback-edit.html', {'form': form})
+
+
+def feedback_delete(request, fid):
+    """删除信息"""
+    f = get_object_or_404(Feedback, pk=fid)
+    f.delete()
+    return redirect(reverse('feedback:feedback_list'))
